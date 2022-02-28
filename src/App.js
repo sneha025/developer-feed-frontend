04e097e94845113de44e7c40ea37131bbbf83495
sneha025 <simaurya96@gmail.com>
2022-02-28 23:04:48 +0530

    added frontend for extension

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Box from "@mui/material/Box";
+import React from "react";
+import NavBar from "./navBar/navBar";
+import image from "./assets/image.png";
+import { Button, Typography } from "@mui/material";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box sx={{ height: "100vh" }}>
+      <NavBar />
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          height: "91vh",
+          backgroundColor: "#393E46",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            p: 6,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h3" sx={{ color: "#FFFFFF" }}>
+            Hi! there Download developer feed chrom extension from{" "}
+          </Typography>
+          <Button variant="contained" sx={{ bgcolor: "#00ADB5" }}>
+            Download Extension
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={image} />
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
